7cf3a4871d03ac09b05fc5c1e8ba9f4df4244d42
Apparao Tiruveedula <131840796+Arjuntiruveedula@users.noreply.github.com>
2024-12-19 08:28:50 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
 import { BsCheckLg } from 'react-icons/bs';
 
-const API_URL = 'https://todo-mern-server-weld.vercel.app';
+const API_URL = 'https://todo-mern-q2er.vercel.app';
 
 function App() {
   const [isCompleteScreen, setIsCompleteScreen] = useState(false);
