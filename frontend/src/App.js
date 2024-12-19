637015b183bf16da990076e24a748435e462f59e
Apparao Tiruveedula <131840796+Arjuntiruveedula@users.noreply.github.com>
2024-12-19 10:00:28 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import { AiOutlineDelete, AiOutlineEdit } from 'react-icons/ai';
 import { BsCheckLg } from 'react-icons/bs';
 
-const API_URL = 'https://todo-mern-q2er.vercel.app';
+const API_URL = 'https://api-ruddy-tau.vercel.app/todos';
 
 function App() {
   const [isCompleteScreen, setIsCompleteScreen] = useState(false);
